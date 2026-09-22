5ad4b41e97a8e2f74f82540a794429ac421fd749
Soumya Shyamali <soumyashyamali6@gmail.com>
2026-09-22 21:13:45 +0530

    Day 1 -rename day1.pt to day1.py

--- day1.py
+++ day1.py
@@ -1,3 +1,26 @@
-print("Hello,GitHub!")
-print("i am starting my ai engineer journey!")
- print("i am excited to learn and grow in this field.")
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+next_year=age+1
+print("Hello", name, "next year you will be", next_year, "years old.")
+
+a=float(input("Enter a number: "))
+b=float(input("Enter another number: "))
+print("sum:", a + b)
+print("difference:", a - b)
+print("product:", a * b)
+print("division:", a / b)
+
+
+
+age = int(input("Enter your age: "))
+if age < 19:
+    print("You are a teenager.")
+else:
+    print("You are adult.")
+
+
+a = int(input("Enter a number: "))
+if a>10:
+   print("big number")
+else:
+    print("small number")    
